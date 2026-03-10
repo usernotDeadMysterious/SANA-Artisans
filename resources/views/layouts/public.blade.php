3678e35daf31dbc51037d4b4f254abf06d710bdc
usernotDeadMysterious <arsalana606@gmail.com>
2026-03-10 12:17:00 +0500

    Meta Tags added to the <head> tag of layout/public: reason Reponsiveness issue solved

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SANA - Skills Academy for Needy Aspirants</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
